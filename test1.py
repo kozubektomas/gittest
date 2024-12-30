6563d28672bad68499f4636b7c7a59d4c3730285
Tomas Kozubek <tomas.kozubek@vsb.cz>
2024-12-30 01:09:18 +0100

    Added plotting

--- test1.py
+++ test1.py
@@ -1,8 +1,15 @@
 import numpy as np
-#import matplotib.pyplot as plt
+import matplotlib.pyplot as plt
 
 x=np.linspace(0,2*np.pi,11)
 y=np.sin(x)
 
 for i in range(0,11):
   print("(%.2f,%.2f)" % (x[i],y[i]))
+
+fig, axis = plt.subplots()
+axis.plot(x,y,'r-')
+axis.set_xlabel('x')
+axis.set_ylabel('y')
+axis.set_title('Test')
+plt.show()
